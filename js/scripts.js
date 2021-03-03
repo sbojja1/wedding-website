f2853258197c54f61d84b6390b6c30c17cf3fff8
sbojja <sbojja@expediagroup.com>
2021-03-03 00:27:59 -0600

    Updated with google script endpoint

--- js/scripts.js
+++ js/scripts.js
@@ -216,7 +216,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxuBFzO4Uvfi-408NBIWXhHXyDV8z0WDGJTLRj_xl1av-xwmPYQwXhX9A/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
